8a2fcb9b186e24d8bc96172a8e5691b6abebbc01
thu1971dlr <thomas.urlings@dlr.de>
2018-04-02 10:45:48 +0200

    testing git

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.DirectoryServices;
+
+/* 
+Create a net461 console application:
+dotnet new console --target-framework-override net461
+
+Add missing assemlby references:
+
+-Ctrl-Shift P (Command Palette)
+-Nuget: Add New Package
+...
+-dotnet restore
+*/
+namespace exe2test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
